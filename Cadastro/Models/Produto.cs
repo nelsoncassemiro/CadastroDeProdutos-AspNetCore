8d63f0bd8c6e50e8b22c28c3f2c5d109d15d8ac9
nelson de sousa cassemiro <nelson.cassemiro@3coracoes.com.br>
2022-01-14 23:21:57 -0300

    crud simples

---
+++ Cadastro/Models/Produto.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Cadastro.Models
+{
+    [Table("Produto")]
+    public class Produto
+    {
+        [Display(Name ="Codigo")]
+        public int Id { get; set; }
+
+        [Display(Name ="Descricao")]
+        public string Descricao { get; set; }
+    }
+}
